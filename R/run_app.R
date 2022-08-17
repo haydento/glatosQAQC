b5cf803f77d92b159a63444a385c384a3745c004
Todd Hayden <thayden@usgs.gov>
2022-08-17 15:08:08 -0400

    fixed default browser issue

--- R/run_app.R
+++ R/run_app.R
@@ -9,5 +9,5 @@ QAQCapp <- function() {
     stop("Could not find app. Try reinstalling `glatosQAQC`.", call. = FALSE)
   }
 
-  shiny::runApp(appDir, display.mode = "normal")
+  shiny::runApp(appDir, display.mode = "normal", launch.browser = TRUE)
 }
